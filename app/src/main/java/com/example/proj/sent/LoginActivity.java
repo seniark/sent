d3b9d77003a9685503726d3b7f23b56a636ae74b
seniark <11bkrain@gmail.com>
2017-12-09 00:19:45 -0500

    FireBase Authentication Added

---
+++ app/src/main/java/com/example/proj/sent/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.example.proj.sent;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LinearLayout myLayout;
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        myLayout = findViewById(R.id.login_layout);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Log.d("LoginActivity", "Already Signed In");
+            goToHome();
+        } else {
+            // not signed in
+            // Choose authentication providers
+
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder().build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                goToHome();
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            showSnackbar(R.string.unknown_sign_in_response);
+        }
+    }
+
+    protected void showSnackbar(int string_id)
+    {
+        Snackbar snackbar = Snackbar.make(myLayout, getString(string_id), Snackbar.LENGTH_SHORT);
+        snackbar.show();
+    }
+
+
+
+    protected void goToHome()
+    {
+        Intent i = new Intent();
+        i.setClass(LoginActivity.this, HomeActivity.class);
+        startActivity(i);
+
+    }
+}
--- app/src/main/java/com/example/proj/sent/UserProfileActivity.java
+++ app/src/main/java/com/example/proj/sent/UserProfileActivity.java
@@ -1,8 +1,16 @@
 package com.example.proj.sent;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class UserProfileActivity extends BaseNavDrawerActivity {
 
@@ -11,5 +19,24 @@ public class UserProfileActivity extends BaseNavDrawerActivity {
         super.onCreate(savedInstanceState);
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.activity_user_profile, contentFrameLayout);
+    }
+
+    protected void signOutClicked(View v)
+    {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        goToSplash();
+                    }
+                });
+
+    }
+
+    protected void goToSplash()
+    {
+        Intent i = new Intent();
+        i.setClass(UserProfileActivity.this, SplashScreenActivity.class);
+        startActivity(i);
     }
 }
